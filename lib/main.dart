49ab20c54bc347f90443d70e7aef3467d1de9b17
Utkarsh3108 <56546503+Utkarsh3108@users.noreply.github.com>
2023-04-17 09:24:51 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'screens/screens.dart';
+import 'config/palette.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Facebook',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData( 
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold
+      ),
+      home: NavScreen(),
+    );
+  }
+}
+
